7cba030cfde900cb5d79a879be947b13e8e1658b
abhishek11125 <103635309+abhishek11125@users.noreply.github.com>
2022-11-10 16:14:15 +0530

    done with day3 tasks

---
+++ Brief Battle Human Resources Management/src/com/brief_battle/model/Employee.java
@@ -0,0 +1,100 @@
+package com.brief_battle.model;
+
+public class Employee {
+	private int employeeID;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String dateOfBirth;
+	private String address;
+	private String gender;
+	private String mobileNumber;
+	private String email;
+	private int deptId;
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Employee(int employeeID, String firstName, String middleName, String lastName, String dateOfBirth,
+			String address, String gender, String mobileNumber, String email, int deptId) {
+		super();
+		this.employeeID = employeeID;
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.dateOfBirth = dateOfBirth;
+		this.address = address;
+		this.gender = gender;
+		this.mobileNumber = mobileNumber;
+		this.email = email;
+		this.deptId = deptId;
+	}
+	public int getEmployeeID() {
+		return employeeID;
+	}
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+	@Override
+	public String toString() {
+		return "Employee [employeeID=" + employeeID + ", firstName=" + firstName + ", middleName=" + middleName
+				+ ", lastName=" + lastName + ", dateOfBirth=" + dateOfBirth + ", address=" + address + ", gender="
+				+ gender + ", mobileNumber=" + mobileNumber + ", email=" + email + ", deptId=" + deptId + "]";
+	}
+	
+
+}
